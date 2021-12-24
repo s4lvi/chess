a999e0be8a23d59eb36c723409067cdfed0b1c3f
s4lvi <salvicomputers@gmail.com>
2021-12-24 12:19:07 -0500

    Added piece image map, added white pieces to board

---
+++ chess-client/src/ChessPiece/images.js
@@ -0,0 +1,14 @@
+export const Images = {
+    "pawnwhite": require('../images/pawnW.png'),
+    "pawnblack": require('../images/pawnB.png'),
+    "rookwhite": require('../images/rookW.png'),
+    "rookblack": require('../images/rookB.png'),
+    "knightwhite": require('../images/knightW.png'),
+    "knightblack": require('../images/knightB.png'),
+    "bishopwhite": require('../images/bishopW.png'),
+    "bishopblack": require('../images/bishopB.png'),
+    "queenwhite": require('../images/queenW.png'),
+    "queenblack": require('../images/queenB.png'),
+    "kingwhite": require('../images/kingW.png'),
+    "kingblack": require('../images/kingB.png')
+}
