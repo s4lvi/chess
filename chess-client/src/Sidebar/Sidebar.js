546b4721e0542de623292f5a6e01f37cb936ca8a
s4lvi <salvicomputers@gmail.com>
2021-12-23 21:55:59 -0500

    Added component skeletons

---
+++ chess-client/src/Sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+class Sidebar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                test
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
